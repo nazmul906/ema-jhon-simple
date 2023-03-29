f29acfd717086a849548fd48924eeee3b5e5333f
finless88 <yanankhan46@gmail.com>
2023-03-30 01:47:09 +0600

    updated

--- src/components/displayData/Products.jsx
+++ src/components/displayData/Products.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./product.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 const Products = (props) => {
-  console.log("inside product", props.product);
+  console.log("inside product", props);
   const { id, category, name, img, price, seller, ratings } = props.product;
+  const handleAddtoCart = props.handleAddtoCart;
+  // onno ekta componet e clicked krle pataite hoile eikhne avabe arrow func likhe hbena
+  //  ekhetre  arrow func ta parent e declare kore thake props hisebe pataite hbe
+  //   const handleClick = (id) => {
+  //     console.log("clicked", id);
+  //   };
+
   return (
     <div className="product">
       {/* remeber eta ke grid kra jabe na .er parent er css file e git krte hbe
@@ -16,7 +24,14 @@ const Products = (props) => {
         <p>manufacture:{seller}</p>
         <p>rating:{ratings}</p>
       </div>
-      <button className="btnCart">Add to cart</button>
+      <button
+        className="btnCart"
+        onClick={() => handleAddtoCart(props.product)}
+      >
+        {/* jeheteo ei click krle product ta onno componet e jabe  */}
+        <FontAwesomeIcon icon={faShoppingCart} />
+        Add to cart
+      </button>
     </div>
   );
 };
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -2,23 +2,42 @@ import React from "react";
 import { useEffect, useState } from "react";
 import Products from "../displayData/Products";
 import "./shop.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 const Shop = () => {
   const [products, setProducts] = useState([]);
+
+  //ei state ta child er click ke track krr
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("productData.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handleAddtoCart = (products) => {
+    console.log("I am beign clicked from Shop.js", products);
+    const newCart = [...cart, products];
+    setCart(newCart);
+  };
+
   return (
     <div className="shopContainer">
       <div className="productContainer">
         {/* <h3>Product here: {products.length} </h3> */}
         {products.map((item) => {
-          return <Products key={item.id} product={item}></Products>;
+          return (
+            <Products
+              key={item.id}
+              product={item}
+              handleAddtoCart={handleAddtoCart}
+            ></Products>
+          );
         })}
       </div>
       <div className="cartContainer">
         <h4>Shop here</h4>
+        <h4>Item:{cart.length}</h4>
       </div>
     </div>
   );
